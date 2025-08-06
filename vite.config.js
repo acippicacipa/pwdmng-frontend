0d870e4bf13cb98d4fe42439a193b59ac7f40e85
acippicacipa <marketing.studio16@gmail.com>
2025-08-06 13:03:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-      '/api': {
-        target: 'https://acippicacipa2.pythonanywhere.com',
-        changeOrigin: true,
-        secure: false,
+  server: {
+    proxy: {
+        '/api': {
+          target: 'https://acippicacipa2.pythonanywhere.com',
+          changeOrigin: true,
+          secure: false,
+        },
       },
-    },
+  },
 })
